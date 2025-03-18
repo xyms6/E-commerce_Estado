b3e25755f1c2b6211d77c8369fd45a6ea4cdee66
Luis. <luis.surf2000@gmail.com>
2025-03-18 16:48:19 -0300

    Update CategoriaDTO.java

--- src/main/java/com/senai/ecommerce/dto/CategoriaDTO.java
+++ src/main/java/com/senai/ecommerce/dto/CategoriaDTO.java
@@ -4,23 +4,21 @@ import com.senai.ecommerce.entities.Categoria;
 
 public class CategoriaDTO {
 
-	private Long id;
-	private String nome;
-	
-	public CategoriaDTO() {
-	}
+    private Long id;
+    private String nome;
 
-	public CategoriaDTO(Long id, String nome) {
-		
-		this.id = id;
-		this.nome = nome;
-	}
-	
-	public CategoriaDTO(Categoria entity) {
-		id = entity.getId();
-		nome = entity.getNome();
-	}
+    public CategoriaDTO() {
+    }
 
+    public CategoriaDTO(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public CategoriaDTO(Categoria entity) {
+        this.id = entity.getId();
+        this.nome = entity.getNome();
+    }
 	public Long getId() {
 		return id;
 	}
