987deee3c43e212ca8a9e6005fdd5fb93e1ecba6
Luis. <luis.surf2000@gmail.com>
2025-03-18 16:46:27 -0300

    Update ProdutoRepository.java

--- src/main/java/com/senai/ecommerce/repositories/ProdutoRepository.java
+++ src/main/java/com/senai/ecommerce/repositories/ProdutoRepository.java
@@ -1,6 +1,9 @@
 package com.senai.ecommerce.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.senai.ecommerce.entities.Produto;
@@ -8,4 +11,6 @@ import com.senai.ecommerce.entities.Produto;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
+    @Query("SELECT DISTINCT p FROM Produto p JOIN FETCH p.categorias")
+    List<Produto> findAllWithCategorias();
 }
