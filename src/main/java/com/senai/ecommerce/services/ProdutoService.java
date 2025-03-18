58f6b72ece964f89ab543996ddb2ffd7f82bc43f
Luis. <luis.surf2000@gmail.com>
2025-03-18 16:46:12 -0300

    Update ProdutoService.java

--- src/main/java/com/senai/ecommerce/services/ProdutoService.java
+++ src/main/java/com/senai/ecommerce/services/ProdutoService.java
@@ -1,40 +1,56 @@
 package com.senai.ecommerce.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.senai.ecommerce.dto.CategoriaDTO;
 import com.senai.ecommerce.dto.ProdutoDTO;
+import com.senai.ecommerce.entities.Categoria;
 import com.senai.ecommerce.entities.Produto;
+import com.senai.ecommerce.repositories.CategoriaRepository;
 import com.senai.ecommerce.repositories.ProdutoRepository;
 
 @Service
 public class ProdutoService {
 
-    @Autowired 
-    private ProdutoRepository repo;
-    @Autowired 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
     private CategoriaRepository categoriaRepository;
-    
+
     public List<ProdutoDTO> buscarTodos() {
-        List<Produto> list = repo.findAll();
-        return list.stream().map(ProdutoDTO::new).toList(); 
+        List<Produto> produtos = produtoRepository.findAllWithCategorias();
+        return produtos.stream().map(ProdutoDTO::new).collect(Collectors.toList());
     }
-    
+
     public Page<ProdutoDTO> buscarPagina(Pageable pagina) {
-        Page<Produto> result = repo.findAll(pagina);
+        Page<Produto> result = produtoRepository.findAll(pagina);
         return result.map(ProdutoDTO::new);
     }
-    
-    public ProdutoDTO criar(ProdutoDTO produtoDTO) {
-        Produto produto = new Produto(produtoDTO);
-        if (produto.getPreco() == null) {
-            produto.setPreco(0.0);
+
+    @Transactional
+    public ProdutoDTO inserir(ProdutoDTO dto) {
+        Produto produto = new Produto();
+        produto.setNome(dto.getNome());
+        produto.setDescricao(dto.getDescricao());
+        produto.setPreco(dto.getPreco());
+        produto.setImgUrl(dto.getImgUrl());
+
+        if (dto.getCategoria() != null) {
+            for (CategoriaDTO cat : dto.getCategoria()) {
+                Categoria categoria = categoriaRepository.getReferenceById(cat.getId());
+                produto.getCategorias().add(categoria);
+            }
         }
-        produto = repo.save(produto); 
-        return new ProdutoDTO(produto); 
+
+        produto = produtoRepository.save(produto);
+        return new ProdutoDTO(produto);
     }
 }
